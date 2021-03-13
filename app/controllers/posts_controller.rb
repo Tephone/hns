517f2bc1e88da420688230e8522285195463764c
naoko kasahara <kassa0918@gmail.com>
2021-03-13 22:10:22 +0900

    辞書機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
     @post.user_id = current_user.id
 
     if @post.save
-      redirect_to posts_path
+      redirect_to posts_path, notice: '投稿しました！'
     else
       render :new
     end
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
 
   def update
     if @post.update(permitted_parameter)
-      redirect_to posts_path
+      redirect_to posts_path, notice: '編集しました！'
     else
       render :edit
     end
@@ -37,7 +37,7 @@ class PostsController < ApplicationController
 
   def destroy
     @post.destroy
-    redirect_to posts_path
+    redirect_to posts_path, notice: '削除しました！'
   end
 
   private
