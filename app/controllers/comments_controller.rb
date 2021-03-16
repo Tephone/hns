7060e4d0ab2d9b1a0c72ae9987259648e17bda7a
naoko kasahara <kassa0918@gmail.com>
2021-03-16 19:10:03 +0900

    コメント編集削除機能（Ajaxによる非同期処理）を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,54 @@
 class CommentsController < ApplicationController
+  before_action :which_post?, only: [:create, :edit, :update]
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to post_path(@post) }
+        # format.html { redirect_to post_path(@post) }
+        format.js { render :index }
       else
         format.html { redirect_to post_path(@post), notice: '投稿できませんでした...' }
       end
     end
   end
 
+  def edit
+    @comment = @post.comments.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'コメント編集中'
+      format.js{ render :edit }
+    end
+  end
+
+  def update
+    @comment = @post.comments.find(params[:id])
+    respond_to do |format|
+      if @comment.update(comment_params)
+        flash.now[:notice] = 'コメントが編集されました'
+        format.js { render :index}
+      else
+        flash.now[:notice] = 'コメントの編集に失敗しました'
+        format.js {render :edit_error}
+      end
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    respond_to do |format|
+      flash.now[:notice] = "コメントが削除されました"
+      format.js { render :index }
+    end
+  end
+
+
   private
   def comment_params
     params.require(:comment).permit(:post_id, :content).merge(user_id: current_user.id)
+  end
+  def which_post?
+    @post = Post.find(params[:post_id])
   end
 end
