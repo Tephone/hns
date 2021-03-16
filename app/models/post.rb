4ecf35610b6db8ac8b9e9a1d775217c21395d977
naoko kasahara <kassa0918@gmail.com>
2021-03-16 22:31:07 +0900

    postモデルにバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
   has_many :favorite_users, through: :favorites, source: :user
   has_many :comments, dependent: :destroy
   mount_uploader :image, ImageUploader
+  validates :content, presence: true
 end
