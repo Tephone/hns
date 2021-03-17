8266aff0a2ebf355c2a37aeb772ae6d2dad564ec
naoko kasahara <kassa0918@gmail.com>
2021-03-17 20:22:51 +0900

    乗馬日誌詳細編集削除機能を追加

--- app/controllers/horse_diaries_controller.rb
+++ app/controllers/horse_diaries_controller.rb
@@ -4,9 +4,14 @@ class HorseDiariesController < ApplicationController
   def index 
     @horse_diaries = HorseDiary.all
   end
+
+  def show
+  end
+
   def new
     @horse_diary = HorseDiary.new
   end
+
   def create
     @horse_diary = HorseDiary.new(permitted_parameter)
     @horse_diary.user = current_user
@@ -15,6 +20,23 @@ class HorseDiariesController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @horse_diary.update(permitted_parameter)
+      redirect_to horse_diaries_path, notice: '編集しました！！'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @horse_diary.destroy
+    puts "@@@@@@@@@@@@@@"
+    redirect_to horse_diaries_path, notice: '削除しました'
   end
   private
   def which_horse_diary?
