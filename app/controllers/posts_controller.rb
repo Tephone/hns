894e58eb9563ed257764cedbc8f2c1c2acbb29d2
naoko kasahara <kassa0918@gmail.com>
2021-03-23 12:11:13 +0900

    post最新投稿が上に来る様に並び替え

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :can_do_only_by_myself, only: [:edit, :update, :destroy]
  
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: :desc)
   end
 
   def show
